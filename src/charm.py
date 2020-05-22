d38c2b160f74bb277c939af286e06127f37e03c5
jamesbeedy <jamesbeedy@gmail.com>
2020-05-22 17:38:22 +0000

    remove unneeded bits from the relation example

--- src/charm.py
+++ src/charm.py
@@ -5,30 +5,14 @@ sys.path.append('lib')
 import logging
 
 from ops.charm import CharmBase
+from ops.framework import Object
 from ops.main import main
-
-from ops.framework import (
-    Object,
-    ObjectEvents,
-    EventSource,
-    EventBase,
-)
 
 
 logger = logging.getLogger()
 
 
-class FooAvailableEvent(EventBase):
-    """Emitted when the value for 'foo' has been received."""
-
-
-class FooEvents(ObjectEvents):
-    foo_available = EventSource(FooAvailableEvent)
-
-
 class FooRequires(Object):
-
-    on = FooEvents()
 
     def __init__(self, charm, relation_name):
         super().__init__(charm, relation_name)
@@ -39,7 +23,6 @@ class FooRequires(Object):
 
     def on_relation_changed(self, event):
         logger.info(event.relation.data[event.unit]['foo'])
-        self.on.foo_available.emit()
 
 
 class FooRequirerCharm(CharmBase):
@@ -50,16 +33,12 @@ class FooRequirerCharm(CharmBase):
         self.framework.observe(self.on.install, self.on_install)
 
         self.foo = FooRequires(self, "foo")
-        self.framework.observe(self.foo.on.foo_available, self.on_foo_available)
 
     def on_start(self, event):
         pass
 
     def on_install(self, event):
         pass
-
-    def on_foo_available(self, event):
-        logging.info("FOO AVAILABLE IN CHARM")
 
 
 if __name__ == "__main__":
